b29eb386924c80eefb6f304965a5dc90def48fa8
VictorChen <victochen@deloitte.com>
2024-09-27 06:20:16 -0500

    Added initial implementation of the USB CDC driver.

---
+++ h563zit6/Drivers/USB_CDC.c
@@ -0,0 +1,261 @@
+/**
+ * @file    USB_CDC.c
+ * @brief   Implementation for the USB CDC (communications device class) driver.
+ * @section Legal Disclaimer
+ *      ©2024 Whisker, All rights reserved. All contents of this source file and/or any other
+ *      related source files are the explicit property of Whisker. Do not distribute. Do not copy.
+ */
+
+/* Includes --------------------------------------------------------------------------------------*/
+
+#include <assert.h>
+#include <stdint.h>
+
+#include "usbd_cdc_if.h"
+#include "usbd_core.h"
+#include "usbd_desc.h"
+#include "USB_CDC.h"
+
+
+/* Internal typedef ------------------------------------------------------------------------------*/
+
+/* Buffer defined before their usage in the USBCDC_Buffers_t definition. Defined early to be used
+ * by the Buffers_t struct. */
+#define TX_BUFFER_SIZE           (512u)
+#define RX_BUFFER_SIZE           (512u)
+
+
+/**
+ * @struct  Buffers_t
+ * @brief   Struct that contains the transmit and receive buffers used in the USB CDC library.
+ * @var Buffers_t::tx Allocated memory for USB transmits.
+ * @var Buffers_t::rx Allocated memory for USB receives.
+ */
+typedef struct {
+    uint8_t txBuffer[TX_BUFFER_SIZE];
+    uint8_t rxBuffer[RX_BUFFER_SIZE];
+} Buffers_t;
+
+
+/**
+ * @struct  USB_CDC
+ * @brief   Type definition of a structure that aggregates key components needed for the USB CDC
+ *          function (as VCP).
+ * @var USB_CDC.pcdHandle       Handle of the USB PCD (peripheral control driver).
+ * @var USB_CDC.usbdHandle      Handle of the USB device class.
+ * @var USB_CDC.usbMutexPtr     Pointer to the USB Mutex.
+ * @var USB_CDC.receiveCallback Receive callback function.
+ */
+typedef struct {
+    PCD_HandleTypeDef pcdHandle;
+    USBD_HandleTypeDef usbdHandle;
+    USB_CDC_ReceiveCallback_t receiveCallback;
+    Buffers_t *buffers;
+    bool initialized;
+    USB_CDC_Err_t retainedError;
+} USB_CDC;
+
+
+/* Internal define -------------------------------------------------------------------------------*/
+
+
+/* Internal macro --------------------------------------------------------------------------------*/
+
+
+/* Internal variables ----------------------------------------------------------------------------*/
+
+/* Singleton instance of the USB buffers. Initialized to 0. */
+static Buffers_t usbBuffers = { 0 };
+
+/* Singleton instance of the USB_CDC driver. All but the buffers are initialized to 0. The buffers
+ * are initialized to point to the usbBuffers instance.
+ */
+static USB_CDC self = {
+    .buffers = &usbBuffers,
+};
+
+
+/* Internal constants ----------------------------------------------------------------------------*/
+
+/* HAL_StatusTypeDef map to USB_CDC_Err_t. */
+static USB_CDC_Err_t const HALErrMap[] = {
+    [HAL_OK]       = USB_CDC_ERR_NONE,
+    [HAL_ERROR]    = USB_CDC_ERR_HAL_BUSY,
+    [HAL_BUSY]     = USB_CDC_ERR_HAL_MEMORY,
+    [HAL_TIMEOUT]  = USB_CDC_ERR_HAL_FAIL,
+};
+
+
+/* USBD_StatusTypeDef map to USB_CDC_Err_t. */
+static USB_CDC_Err_t const USBDErrMap[] = {
+    [USBD_OK]   = USB_CDC_ERR_NONE,
+    [USBD_BUSY] = USB_CDC_ERR_MW_BUSY,
+    [USBD_EMEM] = USB_CDC_ERR_MW_MEMORY,
+    [USBD_FAIL] = USB_CDC_ERR_MW_FAIL,
+};
+
+
+/* Internal function prototypes ------------------------------------------------------------------*/
+
+
+/* Internal functions ----------------------------------------------------------------------------*/
+
+/**
+ * @brief   Initialize the USB device.
+ */
+static USB_CDC_Err_t initUSBD(PCD_HandleTypeDef *const pcdHandle,
+                              USBD_HandleTypeDef *const usbdHandle) {
+    // caller confirms if pcdHandle is not NULL
+
+    pcdHandle->pData = usbdHandle;
+    pcdHandle->Instance = USB_DRD_FS;
+    pcdHandle->Init.dev_endpoints = 8;
+    pcdHandle->Init.speed = USBD_FS_SPEED;
+    pcdHandle->Init.phy_itface = PCD_PHY_EMBEDDED;
+    pcdHandle->Init.Sof_enable = DISABLE;
+    pcdHandle->Init.low_power_enable = DISABLE;
+    pcdHandle->Init.lpm_enable = DISABLE;
+    pcdHandle->Init.battery_charging_enable = DISABLE;
+    pcdHandle->Init.vbus_sensing_enable = DISABLE;
+    pcdHandle->Init.bulk_doublebuffer_enable = DISABLE;
+    pcdHandle->Init.iso_singlebuffer_enable = DISABLE;
+    USB_CDC_Err_t err = HALErrMap[HAL_PCD_Init(pcdHandle)];
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    err = USBDErrMap[USBD_Init(usbdHandle, &CDC_Desc, 0)];
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    err = USBDErrMap[USBD_RegisterClass(usbdHandle, &USBD_CDC)];
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    err = USBDErrMap[USBD_CDC_RegisterInterface(usbdHandle, &USBD_CDC_Template_fops)];
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    err = USBDErrMap[USBD_Start(usbdHandle)];
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    return USB_CDC_ERR_NONE;
+}
+
+
+/* External function prototypes ------------------------------------------------------------------*/
+
+
+/* External functions ----------------------------------------------------------------------------*/
+
+/**
+ * @brief   Initializes the USB CDC driver singleton.
+ * @return  The specific USB_CDC_Err_t which indicates the specific error code if the function
+ *          couldn't be executed successfully. If the function executes successfully,
+ *          USB_CDC_ERR_NONE.
+ */
+USB_CDC_Err_t USB_CDC_Init(void) {
+    if (self.initialized == true) {
+        return USB_CDC_ERR_NONE;
+    }
+    USB_CDC_Err_t err = initUSBD(&self.pcdHandle, &self.usbdHandle);
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    return USB_CDC_ERR_NONE;
+}
+
+
+/**
+ * @brief   Deinitializes the USB CDC driver singleton.
+ * @param[in]   pcdHandle   The handle to the USB PCD (peripheral control driver).
+ * @param[in]   usbdHandle  The handle to the USB device.
+ * @return  The specific USB_CDC_Err_t which indicates the specific error code if the function
+ *          couldn't be executed successfully. If the function executes successfully,
+ *          USB_CDC_ERR_NONE.
+ */
+USB_CDC_Err_t USB_CDC_Deinit(void) {
+    if (self.initialized == false) {
+        return USB_CDC_ERR_NONE;
+    }
+    USB_CDC_Err_t err = HALErrMap[HAL_PCD_DeInit(&self.pcdHandle)];
+    if (err != USB_CDC_ERR_NONE) {
+        return err;
+    }
+    self.initialized = false;
+    return USB_CDC_ERR_NONE;
+}
+
+
+/**
+ * @brief Transmit the specified buffer over the USB bus.
+ * @param[in]   buffer  Buffer of data to transmit.
+ * @param[in]   length  Number of bytes to transmit.
+ * @return  The specific USB_CDC_Err_t which indicates the specific error code if the function
+ *          couldn't be executed successfully. If the function executes successfully,
+ *          USB_CDC_ERR_NONE.
+ */
+USB_CDC_Err_t USB_CDC_Transmit(uint8_t *const buffer, uint16_t length) {
+    if (buffer == NULL) {
+        return USB_CDC_ERR_NULL_PARAM;
+    }
+    if (length == 0u) {
+        return USB_CDC_ERR_INVALID_PARAM;
+    }
+    if (self.usbdHandle.pClassData == NULL) {
+        // USB has not been connected; do not transmit but return with no error
+        return USB_CDC_ERR_NONE;
+    }
+    USB_CDC_Err_t err = USBD_CDC_Transmit(buffer, length);
+    return err;
+}
+
+
+/**
+  * @brief  Initializes the USB PCD MSP.
+  * @param  pcdHandle   The USB PCD handle.
+  * @note   HAL_PCD_MspInit is defined as a weak function in the HAL SDK: see stm32h5xx_hal_pcd.c.
+  *         This implementation will be used over that weak implementation.
+  * @note   The implementation is based on the STM32CubeIDE auto-generated implementation; the only
+  *         addition is the call to HAL_PWREx_EnableVddUSB().
+  */
+void HAL_PCD_MspInit(PCD_HandleTypeDef* pcdHandle) {
+    // caller confirms if pcdHandle is not NULL
+    HAL_PWREx_EnableVddUSB();
+
+    // Initializes the peripherals clock
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {
+        .PeriphClockSelection = RCC_PERIPHCLK_USB,
+        .UsbClockSelection = RCC_USBCLKSOURCE_HSI48,
+    };
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+        assert(false);
+    }
+
+    // USB_DRD_FS clock enable
+    __HAL_RCC_USB_CLK_ENABLE();
+
+    // USB_DRD_FS interrupt init
+    HAL_NVIC_SetPriority(USB_DRD_FS_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(USB_DRD_FS_IRQn);
+    return;
+}
+
+
+/**
+  * @brief  DeInitializes USB PCD MSP.
+  * @param  pcdHandle   The USB PCD handle
+  * @note   HAL_PCD_MspDeInit is defined as a weak function in the HAL SDK: see stm32h5xx_hal_pcd.c.
+  *         This implementation will be used over that weak implementation.
+  * @note   The implementation is based on the STM32CubeIDE auto-generated implementation; the only
+  *         addition is the call to HAL_PWREx_DisableeVddUSB().
+  */
+void HAL_PCD_MspDeInit(PCD_HandleTypeDef* pcdHandle) {
+    // caller confirms if pcdHandle is not NULL
+
+    HAL_PWREx_DisableVddUSB();
+    // peripheral clock disable
+    __HAL_RCC_USB_CLK_DISABLE();
+    // USB_DRD_FS interrupt deinit
+    HAL_NVIC_DisableIRQ(USB_DRD_FS_IRQn);
+}
--- h563zit6/Drivers/USB_CDC.h
+++ h563zit6/Drivers/USB_CDC.h
@@ -1,5 +1,5 @@
 /**
- * @file    USBCDC.h
+ * @file    USB_CDC.h
  * @brief   Interface file for the USB CDC (communications device class) driver.
  * @note    The driver is reentrant from the defined structures; the driver may not be reentrant
  *          if different instances of the structures are constructed using the same HW peripherals,
@@ -9,8 +9,8 @@
  *      ©2024 Whisker, All rights reserved. All contents of this source file and/or any other
  *      related source files are the explicit property of Whisker. Do not distribute. Do not copy.
  */
-#ifndef USBCDC_H_
-#define USBCDC_H_
+#ifndef USB_CDC_H_
+#define USB_CDC_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -21,13 +21,33 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "Mutex.h"
 #include "stm32h5xx_hal.h"
-#include "usbd_cdc_if.h"
-#include "usbd_core.h"
+#include "usbd_def.h"
+
 
 
 /* External typedef ------------------------------------------------------------------------------*/
+
+/**
+ * @enum    USB_CDC_Err_t
+ * @brief   Enumeration of the different USB CDC driver function return error codes.
+ */
+typedef enum {
+    USB_CDC_ERR_NONE = 0u,          /*!< No error */
+    USB_CDC_ERR_NULL_PARAM,         /*!< An input parameter is NULL which is invalid for that
+                                         parameter; assert checks should catch these (possibly
+                                         UNUSED) */
+    USB_CDC_ERR_INVALID_PARAM,      /*!< An input parameter had an invalid value */
+    USB_CDC_ERR_RESOURCE_BLOCKED,   /*!< The HW resource is currently blocked */
+    USB_CDC_ERR_UNINITIALIZED,      /*!< The USB CDC is not initialized */
+    USB_CDC_ERR_HAL_BUSY,           /*!< HAL-level busy error */
+    USB_CDC_ERR_HAL_MEMORY,         /*!< HAL-level memory access error */
+    USB_CDC_ERR_HAL_FAIL,           /*!< HAL-level general failure/error */
+    USB_CDC_ERR_MW_BUSY,            /*!< USBD middleware busy error */
+    USB_CDC_ERR_MW_MEMORY,          /*!< USBD middleware memory access error */
+    USB_CDC_ERR_MW_FAIL,            /*!< USBD middleware general failure/error */
+} USB_CDC_Err_t;
+
 
 /**
  * @brief   USB CDC receive callback function that is invoked when data is received over USB.
@@ -36,41 +56,7 @@ extern "C" {
  * @return  The number of data bytes received that were processed; ideally should be equal to the
  *          length parameter.
  */
-typedef uint16_t (*USBCDC_ReceiveCallback_t)(uint8_t *const buffer, uint16_t length);
-
-
-/* Buffer defined before their usage in the USBCDC_Buffers_t definition. */
-#define USBCDC_TX_BUFFER_SIZE           (512u)
-#define USBCDC_RX_BUFFER_SIZE           (512u)
-
-
-/**
- * @struct  USBCDC_Buffers_t
- * @brief   Struct that contains the transmit and receive buffers used in the USB CDC library.
- * @var USBCDC_Buffers_t.txBuffer
- */
-typedef struct {
-    uint8_t txBuffer[USBCDC_TX_BUFFER_SIZE];
-    uint8_t rxBuffer[USBCDC_RX_BUFFER_SIZE];
-} USBCDC_Buffers_t;
-
-
-/**
- * @struct  USBCDC
- * @brief   Type definition of a structure that aggregates key components needed for the USB CDC
- *          function (as VCP).
- * @var USBCDC.pcdHandle        Handle of the USB PCD (peripheral control driver).
- * @var USBCDC.usbdHandle       Handle of the USB device class.
- * @var USBCDC.usbMutexPtr      Pointer to the USB Mutex.
- * @var USBCDC.receiveCallback  Receive callback function.
- */
-typedef struct {
-    PCD_HandleTypeDef *pcdHandle;
-    USBD_HandleTypeDef *usbdHandle;
-    Mutex *usbMutexPtr;
-    USBCDC_ReceiveCallback_t receiveCallback;
-    USBCDC_Buffers_t *buffers;
-} USBCDC;
+typedef uint16_t (*USB_CDC_ReceiveCallback_t)(uint8_t *const buffer, uint16_t length);
 
 
 /* Defines ---------------------------------------------------------------------------------------*/
@@ -87,15 +73,13 @@ typedef struct {
 
 /* External functions ----------------------------------------------------------------------------*/
 
-USBCDC USBCDC_ctor(PCD_HandleTypeDef *const pcdHandle, USBD_HandleTypeDef *const usbdHandle,
-                   Mutex *const usbMutexPtr, USBCDC_ReceiveCallback_t receiveCallback,
-                   USBCDC_Buffers_t *buffers);
-bool USBCDC_Init(USBCDC const *const self);
-bool USBCDC_Transmit(USBCDC const *const self, uint8_t const *const buffer, uint16_t length);
+USB_CDC_Err_t USB_CDC_Init(void);
+USB_CDC_Err_t USB_CDC_Deinit(void);
+USB_CDC_Err_t USB_CDC_Transmit(uint8_t *const buffer, uint16_t length);
 
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* USBCDC */
+#endif /* USB_CDC_H_ */
